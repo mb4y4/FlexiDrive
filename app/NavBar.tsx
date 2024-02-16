b1e7e85c25897e2955014544dd618e1626f53ed4
Mb4y4 <d.sommy@gmail.com>
2024-02-16 15:44:20 +0300

    Navbar styling

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,16 +3,14 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div  className="flex items-center justify-between px-4 py-2 bg-white shadow-md">
-        <img src="/logo2.jpg" alt="logo"  style={{width: "7%"}} />
-        <div className='flex space-x-3 items-center'>
-            <h2 className='hover:bg-blue-400 px-3 cursor-pointer p-2 rounded-full hover:text-white'>Home</h2>
-            <h2 className='hover:bg-blue-400 px-3 cursor-pointer p-2 rounded-full hover:text-white'>History</h2>
-            <h2 className='hover:bg-blue-400 px-3 cursor-pointer p-2 rounded-full hover:text-white'>Contact Us</h2>
-            <div>
-                <UserButton afterSignOutUrl="/"/>
-            </div>
+    <div className="flex items-center justify-between p-3 px-5 bg-white shadow-md border-b-[1px]">
+        <img src="/logo2.jpg" alt="logo" height={100} width={100} />
+        <div className='hidden md:flex space-x-3 items-center'>
+            <h2 className='hover:bg-blue-400 px-2 cursor-pointer p-1 rounded-full hover:text-white'>Home</h2>
+            <h2 className='hover:bg-blue-400 px-2 cursor-pointer p-1 rounded-full hover:text-white'>History</h2>
+            <h2 className='hover:bg-blue-400 px-2 cursor-pointer p-1 rounded-full hover:text-white'>Contact Us</h2>
         </div>
+        <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
